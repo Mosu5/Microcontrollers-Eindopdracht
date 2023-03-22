a12fe3c9e2fdc34e60de7da09cf592e1b1b8e7ec
m05u <90831787+m05u@users.noreply.github.com>
2023-03-22 08:49:35 +0100

    [ADD] init project files
    Created microchip studio project

---
+++ theremin-project/theremin-project/main.c
@@ -0,0 +1,18 @@
+/*
+ * theremin-project.c
+ *
+ * Created: 22-3-2023 08:45:33
+ * Author : moust
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
