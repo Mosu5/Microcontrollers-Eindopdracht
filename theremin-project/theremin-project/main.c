81671101089b42a455bc8e5db2797079f3f0f08a
Moustapha Azaimi <m.azaimi@student.avans.nl>
2023-03-22 14:32:28 +0100

    [EDIT] added pitch to buzzer

--- theremin-project/theremin-project/main.c
+++ theremin-project/theremin-project/main.c
@@ -8,34 +8,17 @@
 #define BIT(x) (1 << (x))
 #define INTERVAL 2273
 
+void wait(int ms);
+void wait_us(int us);
+void buzzerOn(uint16_t duration);
+void set_buzzer_pitch(uint16_t pitch);
+void turn_off_buzzer();
 unsigned int sCount = 0, minutes = 0, hours = 0;
-
-// wait(): busy waiting for 'ms' millisecond
-// Used library: util/delay.h
-void wait(int ms)
-{
-	for (int i = 0; i < ms; i++)
-	{
-		_delay_ms(1); // library function (max 30 ms at 8MHz)
-	}
-}
-
-// Initialize timer 1: fast PWM at pin PORTB.6 (hundredth ms)
-void timer1Init(void)
-{
-	ICR1 = INTERVAL;	  // TOP value for counting = INTERVAL*us
-	OCR1A = INTERVAL / 2; // compare value in between
-	TCCR1A = 0b10000010;  // timer, compare output at OC1A=PB5
-	TCCR1B = 0b00011010;  // fast PWM, TOP = ICR1, prescaler=8 (1MHz), RUN
-}
-
-void toggle_buzz()
-{
-	PORTA ^= BIT(0);
-}
 
 #define TRIG PD5
 #define ECHO PD4
+
+#define BUZZ PA0
 
 int main(void)
 {
@@ -50,50 +33,97 @@ int main(void)
 	lcd_clear();
 	lcd_write_string("Hello World!");
 
+	int watchdog = 0;
+	duration = 0;
+	
+	set_buzzer_pitch(440);
+
 	while (1)
 	{
-		// listen to a button press on PORTB.0
-		// if button is pressed, init lcd
-		if (
-			(PINB & (1 << 0)) == 0)
-		{
-			init_4bits_mode();
-			_delay_ms(10000);
-			lcd_clear();
-			lcd_write_string("Hello World!");
-		}
-
 		PORTD |= (1 << TRIG); // set PORTD.5 to output high
 		_delay_us(40);
 		PORTD &= ~(1 << TRIG);
-		_delay_us(25000);
+		_delay_us(40);
 
-		// when pulse starts high start timer
-		// stop  timer when pulse starts low
-		// save duration for buzz
+		// while echo is high, count
+		while (PIND & (1 << ECHO))
+		{
+			watchdog++;
+			if (watchdog > 20000)
+			{
+				break;
+			}
+			duration++;
+			_delay_us(1);
+		}
 
-		// empty while loop until pulse starts high
-		while ((PIND & (1 << ECHO)) == 0)
-			;
-
-		// start timer
-		timer1Init();
-
-		// empty while loop until pulse starts low
-		while ((PIND & (1 << ECHO)) != 0)
-			;
-
-		// save the duration of the pulse in duration
-		duration = ICR1;
-
-		// write duration to lcd
-		lcd_clear();
-		lcd_write_string("duration: ");
-		lcd_write_integer(duration);
-
-		// toggle buzz with duration as delay
-		toggle_buzz();
-		wait(duration);
-		toggle_buzz();
+		// when the echo is low, stop counting, set the buzzer frequency and reset the duration
+		if (watchdog < 20000)
+		{
+			buzzerOn(duration);
+		}
+		duration = 0;
+		_delay_us(30000); // wait until echo times out
 	}
+}
+
+// wait(): busy waiting for 'ms' millisecond
+// Used library: util/delay.h
+void wait(int ms)
+{
+	for (int i = 0; i < ms; i++)
+	{
+		wait(1); // library function (max 30 ms at 8MHz)
+	}
+}
+
+void wait_us(int us)
+{
+	for (int i = 0; i < us; i++)
+	{
+		_delay_us(1); // library function (max 30 ms at 8MHz)
+	}
+}
+
+// buzzerOn(): turns the buzzer on for a certain duration
+void buzzerOn(uint16_t duration)
+{
+	// set the buzzer frequency
+	set_buzzer_pitch(duration);
+
+	// turn the buzzer on
+	PORTA |= (1 << BUZZ);
+
+	// wait for the duration
+	wait_us((int)duration);
+
+	// turn the buzzer off
+	PORTA &= ~(1 << BUZZ);
+}
+
+// // set_buzzer_pitch(): sets the buzzer frequency in Hz (pitch)
+// void set_buzzer_pitch(uint16_t pitch)
+// {
+// 	// set the buzzer frequency
+// 	OCR1A = INTERVAL / pitch;
+// }
+
+void set_buzzer_pitch(uint16_t pitch)
+{
+	// Set up Timer/Counter0 for PWM output on OC0A (PD6)
+	DDRD |= (1 << PD6);
+	TCCR1A = (1 << COM1A0) | (2 << WGM00);
+	TCCR1B = (3 << CS00);
+
+	// Set the frequency
+	OCR1A = F_CPU / (2 * pitch) - 1;
+
+	// turn the buzzer on
+	PORTA |= (1 << BUZZ);
+}
+
+void turn_off_buzzer()
+{
+	// turn the buzzer off
+	PORTA &= ~(1 << BUZZ);
 }
